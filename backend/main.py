ef45a3419b0372655abd1e1e22d477e7d4c6900e
Lina Sahbani <lina.sahbani04@gmail.com>
2026-08-02 18:40:06 +0200

    Module 1 : endpoint upload de documents fonctionnel

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-from fastapi import Depends
+from fastapi import FastAPI, Depends, UploadFile, File
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from typing import List
+import shutil
+import os
+
 import models
 import schemas
-from database import get_db
-
+from database import get_db, engine
 
 app = FastAPI(title="Banking Compliance & Audit Assistant API")
 
@@ -17,9 +19,6 @@ def read_root():
 def health_check():
     return {"status": "ok"}
 
-from database import engine
-from sqlalchemy import text
-
 @app.get("/test-db")
 def test_db():
     with engine.connect() as connection:
@@ -29,3 +28,24 @@ def test_db():
 @app.get("/api/documents", response_model=List[schemas.DocumentOut])
 def list_documents(db: Session = Depends(get_db)):
     return db.query(models.Document).all()
+
+UPLOAD_DIR = "uploaded_files"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.post("/api/documents", response_model=schemas.DocumentOut)
+def upload_document(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    nouveau_document = models.Document(
+        nom=file.filename,
+        type=file.content_type,
+        chemin_fichier=file_path
+    )
+    db.add(nouveau_document)
+    db.commit()
+    db.refresh(nouveau_document)
+
+    return nouveau_document
